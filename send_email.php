97e7e90052a0e872a4f577ca5d48e6db96a0844d
agusdom <agustindominguez95@hotmail.com>
2024-07-11 01:16:16 -0300

    .

--- send_email.php
+++ send_email.php
@@ -1,5 +1,8 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
